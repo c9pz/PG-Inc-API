761d2eadc739e41c01967fd66820ff3417fcf2d4
c9pz <119216428+c9pz@users.noreply.github.com>
2024-12-30 19:58:43 +0200

    Update RAP.js

--- pages/api/RAP.js
+++ pages/api/RAP.js
@@ -3,32 +3,33 @@ import fetch from 'node-fetch';
 export default async function handler(req, res) {
     const { userId } = req.query;
 
-    if (!userId) {
-        res.status(400).json({ error: 'UserId is required' });
-        return;
-    }
-
     try {
-        const response = await fetch(`https://inventory.roblox.com/v1/users/${userId}/assets/collectibles?limit=100`);
-        const data = await response.json();
-
-        if (response.status !== 200 || !data.data) {
-            res.status(500).json({ error: 'Failed' });
-            return;
-        }
-
         let totalRAP = 0;
+        let cursor = '';
+        const limit = 100;
 
-        for (const item of data.data) {
-            if (item.recentAveragePrice) {
-                totalRAP += item.recentAveragePrice;
+        while (true) {
+            const response = await fetch(`https://inventory.roblox.com/v1/users/${userId}/assets/collectibles?limit=${limit}&cursor=${cursor}`);
+            const data = await response.json();
+
+            if (response.status !== 200) {
+                res.status(500).json({ error: 'Error' });
+                return;
             }
+
+            if (!data.data || data.data.length === 0) break;
+
+            totalRAP += data.data.reduce((acc, item) => acc + (item.recentAveragePrice || 0), 0);
+
+            if (!data.nextPageCursor) break;
+
+            cursor = data.nextPageCursor;
         }
 
-        res.status(200).json({ playerRAP: totalRAP });
+        const formattedRAP = totalRAP.toLocaleString();
+        res.status(200).json({ playerRAP: formattedRAP });
 
     } catch (error) {
-        console.error('Error fetching RAP:', error);
         res.status(500).json({ error: 'Error' });
     }
 }
