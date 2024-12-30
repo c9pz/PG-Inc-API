6df227f50fd349fa914d0e2ea6e0761d3092d928
c9pz <119216428+c9pz@users.noreply.github.com>
2024-12-30 16:18:25 +0200

    Update getItemCount.js

--- pages/api/getItemCount.js
+++ pages/api/getItemCount.js
@@ -1,27 +1,32 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const { userId } = req.query
-  const url = `https://inventory.roblox.com/v1/users/${userId}/assets/collectibles?limit=9999999`
+    const { userId } = req.query;
 
-  try {
-    const response = await fetch(url)
-    
-    if (!response.ok) {
-      const errorMessage = await response.text()
-      console.error('Error response from API:', errorMessage)
-      return res.status(response.status).json({ error: 'Failed to fetch data' })
+    let allItems = [];
+    let page = 1;
+    const limit = 99999999;
+
+    try {
+        while (true) {
+            const response = await fetch(`https://inventory.roblox.com/v1/users/${userId}/assets/collectibles?limit=${limit}&page=${page}`);
+            const data = await response.json();
+
+            if (data.data.length > 0) {
+                allItems = [...allItems, ...data.data];
+            }
+
+            if (!data.nextPage) {
+                break;
+            }
+
+            page++;
+        }
+
+        res.status(200).json({ itemCount: allItems.length });
+
+    } catch (error) {
+        console.error('Error fetching items:', error);
+        res.status(500).json({ error: 'Failed to fetch inventory items' });
     }
-
-    const data = await response.json()
-
-    if (data.errors && data.errors[0].code === 10) {
-      return res.status(200).json({ itemCount: 'Private' })
-    }
-
-    res.status(200).json({ itemCount: data.data.length })
-  } catch (error) {
-    console.error('Error during fetch:', error)
-    res.status(500).json({ error: 'Internal server error' })
-  }
 }
