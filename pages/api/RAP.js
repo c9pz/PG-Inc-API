15b87d1483fbb9ec33b352a7f7ecc8e1335fc4cd
c9pz <119216428+c9pz@users.noreply.github.com>
2024-12-30 20:15:58 +0200

    Update RAP.js

--- pages/api/RAP.js
+++ pages/api/RAP.js
@@ -12,21 +12,27 @@ export default async function handler(req, res) {
         const userItemsData = await userItemsResponse.json();
 
         if (userItemsResponse.status !== 200) {
-            return res.status(500).json({ error: 'Error' });
+            return res.status(500).json({ error: 'Error fetching items' });
         }
 
         let rap = 0;
+        let validItemCount = 0;
+
         for (const item of userItemsData.data) {
             const itemDetailsResponse = await fetch(`https://catalog.roblox.com/v1/items/${item.id}/details`);
             const itemDetailsData = await itemDetailsResponse.json();
 
             if (itemDetailsResponse.status === 200 && itemDetailsData.price) {
                 rap += itemDetailsData.price;
+                validItemCount++;
             }
         }
 
-        const formattedRAP = rap.toLocaleString();
+        if (validItemCount === 0) {
+            return res.status(404).json({ error: 'No valid items with RAP found' });
+        }
 
+        const formattedRAP = rap.toLocaleString();
         res.status(200).json({ playerRAP: formattedRAP });
 
     } catch (error) {
