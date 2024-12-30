764528e9edd9beb0e48313e55a08cdb94d08bdda
c9pz <119216428+c9pz@users.noreply.github.com>
2024-12-30 16:11:45 +0200

    Update getItemCount.js

--- pages/api/getItemCount.js
+++ pages/api/getItemCount.js
@@ -3,11 +3,13 @@ import fetch from 'node-fetch'
 export default async function handler(req, res) {
   const { userId } = req.query
   const url = `https://inventory.roblox.com/v1/users/${userId}/assets/collectibles?limit=9999999`
-  
+
   try {
     const response = await fetch(url)
-
+    
     if (!response.ok) {
+      const errorMessage = await response.text()
+      console.error('Error response from API:', errorMessage)
       return res.status(response.status).json({ error: 'Failed to fetch data' })
     }
 
@@ -19,6 +21,7 @@ export default async function handler(req, res) {
 
     res.status(200).json({ itemCount: data.data.length })
   } catch (error) {
+    console.error('Error during fetch:', error)
     res.status(500).json({ error: 'Internal server error' })
   }
 }
