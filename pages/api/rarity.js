edd516299f1c7e6c9baad30482c76abb7f7356f8
c9pz <119216428+c9pz@users.noreply.github.com>
2024-12-31 13:17:29 +0200

    Update rarity.js

--- pages/api/rarity.js
+++ pages/api/rarity.js
@@ -11,17 +11,20 @@ export default async function handler(req, res) {
         const response = await fetch(`https://api.roblox.com/marketplace/productinfo?assetId=${itemId}`);
         const data = await response.json();
 
-        if (response.status !== 200 || !data) {
+        if (!data || data.error) {
             return res.status(500).json({ error: 'Error fetching item details' });
         }
 
         const itemName = data.Name;
         const isLimited = data.IsLimited;
+        const isLimitedU = data.IsLimitedUnique;
         const price = data.PriceInRobux;
 
         let rarityRank = "Unknown";
 
-        if (isLimited) {
+        if (isLimitedU) {
+            rarityRank = "Limited U";
+        } else if (isLimited) {
             rarityRank = "Limited";
         } else if (price >= 5000) {
             rarityRank = "Epic";
@@ -39,6 +42,7 @@ export default async function handler(req, res) {
             rarityRank,
             price
         });
+
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Error fetching item details' });
